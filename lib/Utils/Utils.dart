2e3787d1a9a10391091169b0e6832176ef24ce68
ijas-cookee <ijas@cookee.io>
2021-06-26 21:31:04 +0530

    first commit

---
+++ lib/Utils/Utils.dart
@@ -0,0 +1,5 @@
+class Utils {
+  static String getAssetPng(String fileName) => "assets/images/$fileName.png";
+  static String getAssetSvg(String fileName) => "assets/images/$fileName.svg";
+  static String getAssetJpg(String fileName) => "assets/images/$fileName.jpg";
+}
